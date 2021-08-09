c4bdc522084dec79923539c327ff4bc0119f839b
AshakaE <ashakaegerega@gmail.com>
2021-08-09 23:48:05 +0100

    Add style for contact form

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
-let menu = document.getElementById('toggle-icon');
-let panel = document.getElementById('data-panel');
-let list = document.getElementById('data-list');
-let one = document.getElementById('one');
-let two = document.getElementById('two');
-let three = document.getElementById('three');
+let menu = document.getElementById("toggle-icon")
+let panel = document.getElementById("data-panel")
+let list = document.getElementById("data-list")
+let one = document.getElementById("one")
+let two = document.getElementById("two")
+let three = document.getElementById("three")
 
 const clickFunction = () => {
-  menu.classList.toggle('rotate-icon');
-};
+  menu.classList.toggle("rotate-icon")
+}
 
-window.addEventListener('load', function () {
-  let direction = '';
-  list.classList.add('list-items');
+window.addEventListener("load", function () {
+  let direction = ""
+  list.classList.add("list-items")
 
-  menu.addEventListener('click', function () {
-    list.classList.remove('list-items');
-    panel.classList.remove('disp');
-    list.classList.remove('data-list');
-    one.classList.remove('one');
-    two.classList.remove('two');
-    three.classList.remove('three');
+  menu.addEventListener("click", function () {
+    list.classList.remove("list-items")
+    panel.classList.remove("disp")
+    list.classList.remove("data-list")
+    one.classList.remove("one")
+    two.classList.remove("two")
+    three.classList.remove("three")
 
-    panel.offsetWidth = panel.offsetWidth;
+    panel.offsetWidth = panel.offsetWidth
 
-    if (direction === 'toRight') {
-      direction = 'toLeft';
-      panel.style.animationDirection = 'reverse';
-      list.style.animationDirection = 'reverse';
+    if (direction === "toRight") {
+      direction = "toLeft"
+      panel.style.animationDirection = "reverse"
+      list.style.animationDirection = "reverse"
 
-      one.style.animationDirection = 'reverse';
-      two.style.animationDirection = 'reverse';
-      three.style.animationDirection = 'reverse';
+      one.style.animationDirection = "reverse"
+      two.style.animationDirection = "reverse"
+      three.style.animationDirection = "reverse"
     } else {
-      direction = 'toRight';
-      panel.style.animationDirection = '';
-      list.style.animationDirection = '';
+      direction = "toRight"
+      panel.style.animationDirection = ""
+      list.style.animationDirection = ""
 
-      one.style.animationDirection = '';
-      two.style.animationDirection = '';
-      three.style.animationDirection = '';
+      one.style.animationDirection = ""
+      two.style.animationDirection = ""
+      three.style.animationDirection = ""
     }
 
-    panel.classList.add('disp');
-    list.classList.add('data-list');
+    panel.classList.add("disp")
+    list.classList.add("data-list")
 
-    one.classList.add('one');
-    two.classList.add('two');
-    three.classList.add('three');
-  });
-});
+    one.classList.add("one")
+    two.classList.add("two")
+    three.classList.add("three")
+  })
+})
