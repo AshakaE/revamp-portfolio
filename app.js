cbecdefa41b4162e06b67a1bbc1035e240039ca5
AshakaE <ashakaegerega@gmail.com>
2021-08-16 14:43:47 +0100

    Add scroll effect

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
 let menu = document.getElementById("toggle-icon")
 let panel = document.getElementById("data-panel")
 let list = document.getElementById("data-list")
 let one = document.getElementById("one")
 let two = document.getElementById("two")
 let three = document.getElementById("three")
-let rev = [panel, list, one, two, three]
+anchorlinks = document.querySelectorAll('a[href^="#"]')
+let portfolio = document.getElementById("portfolio")
+let contact = document.getElementById("contact")
+let all = [one, two, three]
+let rev = [panel, list, ...all]
+let cont = [menu, ...all]
 
 window.addEventListener("load", function () {
   let direction = ""
   list.classList.add("list-items")
 
-  menu.addEventListener("click", function () {
-    menu.classList.toggle("rotate-icon")
-    list.classList.remove("list-items")
-    list.classList.remove("data-list")
+  cont.forEach((el) =>
+    el.addEventListener("click", function () {
+      menu.classList.toggle("rotate-icon")
+      list.classList.remove("list-items")
+      list.classList.remove("data-list")
 
-    panel.classList.remove("disp")
+      panel.classList.remove("disp")
 
-    one.classList.remove("one")
-    two.classList.remove("two")
-    three.classList.remove("three")
+      one.classList.remove("one")
+      two.classList.remove("two")
+      three.classList.remove("three")
 
-    panel.offsetWidth = panel.offsetWidth
+      panel.offsetWidth = panel.offsetWidth
 
-    if (direction === "toRight") {
-      direction = "toLeft"
-      rev.forEach((el) => (el.style.animationDirection = "reverse"))
-    } else {
-      direction = "toRight"
-      rev.forEach((el) => (el.style.animationDirection = ""))
-    }
+      if (direction === "toRight") {
+        direction = "toLeft"
+        rev.forEach((el) => (el.style.animationDirection = "reverse"))
+      } else {
+        direction = "toRight"
+        rev.forEach((el) => (el.style.animationDirection = ""))
+      }
 
-    list.classList.add("data-list")
-    panel.classList.add("disp")
+      list.classList.add("data-list")
+      panel.classList.add("disp")
 
-    one.classList.add("one")
-    two.classList.add("two")
-    three.classList.add("three")
-  })
+      one.classList.add("one")
+      two.classList.add("two")
+      three.classList.add("three")
+    })
+  )
 })
+
+for (let item of anchorlinks) {
+  item.addEventListener("click", (e) => {
+    let hashval = item.getAttribute("href")
+    let target = document.querySelector(hashval)
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    })
+    history.pushState(null, null, hashval)
+    e.preventDefault()
+  })
+}
