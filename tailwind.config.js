cf4e1e70679bf85fc55ed4ef528ffab60969d1d2
AshakaE <ashakaegerega@gmail.com>
2021-09-22 12:12:22 +0100

    Complete service section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,32 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./**/*.html"],
+  mode: 'jit',
+  purge: ['./**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        primaryLight: "#3747E5",
-        primary: "#1C2AAF",
-        primaryDark: "#0E1879",
-        textLight: "#D1D6FE",
-        textDark: "#5C6086",
-        bodyDark: "#E9ECFF",
+        primaryLight: '#3747E5',
+        primary: '#1C2AAF',
+        primaryDark: '#0E1879',
+        textLight: '#D1D6FE',
+        textBg: '#2B37BD',
+        textDark: '#5C6086',
+        bodyDark: '#E9ECFF',
       },
       fontFamily: {
-        head: ["poppins"],
-        body: ["noto sans"],
+        head: ['poppins'],
+        body: ['noto sans'],
       },
       boxShadow: {
-        scoop: "0 50px 0 0 #1C2AAF",
-        troop: "0 50px 0 0 #D1D6FE",
-        droop: "0 50px 0 0 #FFFFFF",
+        scoop: '0 50px 0 0 #1C2AAF',
+        troop: '0 50px 0 0 #D1D6FE',
+        droop: '0 50px 0 0 #FFFFFF',
       },
       borderRadius: {
-        "4xl": "3.5rem",
+        '4xl': '3.5rem',
       },
       transitionTimingFunction: {
-        "menu-expo": "cubic-bezier(0.87, 0, 0.13, 1)",
+        'menu-expo': 'cubic-bezier(0.87, 0, 0.13, 1)',
       },
     },
   },
@@ -33,4 +34,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
