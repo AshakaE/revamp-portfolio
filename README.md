821deed7e823c4853e2cdb497539b0d7d4c297df
AshakaE <ashakaegerega@gmail.com>
2021-10-06 23:25:30 +0100

    Update contact form

---
+++ README.md
@@ -0,0 +1,65 @@
+# AshakaE
+
+> A full overhaul of my portfolio that dynamically gets my pinned projects from GitHub using the Graphql API and a working contact form.
+
+<p align="center">
+  
+  <img src="docs/snapshot1.png">
+
+</p>
+
+## Built With
+
+- HTML
+- CSS
+- TailwindCSS
+- Node.js
+- Express.js
+
+## Live Demo
+
+[AshakaE](https://ashakae.herokuapp.com/)
+
+## Getting Started
+
+- `git clone git@github.com:AshakaE/revamp-portfolio.git`
+- `cd revamp-portfolio`
+- `npm install`
+- `npm start`
+
+### Prerequisites
+
+- Node.js
+- Get Github [Personal access token](https://github.com/settings/tokens)
+- Get Google [OAuth2 token](https://developers.google.com/gmail/api/quickstart/nodejs)
+
+## Author
+
+👤 **Ashaka Egerega**
+
+- GitHub: [@AshakaE](https://github.com/AshakaE)
+- LinkedIn: [AshakaE](https://www.linkedin.com/in/AshakaE/)
+
+## 🤝 Contributing
+
+Contributions, issues and, feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/AshakaE/revamp-portfolio/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- [Well Rafi](https://www.behance.net/wellrafiui) for the [Original design](https://www.behance.net/gallery/109004667/Personal-Portfolio-Website-Inspiration?tracking_source=search_projects_recommended%7Cdeveloper%20portfolio%20personal%20website&)
+
+## 📝 License
+
+Copyright 2021 Ashaka Egerega
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS, OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import fetch from "node-fetch"
 import cors from "cors"
+import nodemailer from "nodemailer"
+import multiparty from "multiparty"
 import dotenv from "dotenv"
 
 dotenv.config()
@@ -11,6 +13,53 @@ app.use(express.static("public"))
 app.set("view engine", "ejs")
 
 const PORT = process.env.PORT || 7000
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.EMAIL,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+    accessToken: process.env.ACCESS_TOKEN,
+    expires: process.env.EXPIRY_DATE,
+  },
+})
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log("Server is ready to take our messages")
+  }
+})
+
+app.post("/send", (req, res) => {
+  let form = new multiparty.Form()
+  let data = {}
+  form.parse(req, function (err, fields) {
+    console.log(fields)
+    Object.keys(fields).forEach(function (property) {
+      data[property] = fields[property].toString()
+    })
+    console.log(data)
+    const mail = {
+      sender: `Mail from ${data.email} - ${data.name}`,
+      to: process.env.EMAIL,
+      subject: data.subject,
+      text: `\n ${data.name} <${data.email}> \n${data.message}`,
+    }
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        console.log(err)
+        res.status(500).send("Something went wrong.")
+      } else {
+        res.status(200).send("Email successfully sent to recipient!")
+      }
+    })
+  })
+})
 
 app.route("/").get(function (req, res) {
   let content = {
@@ -50,10 +99,6 @@ app.route("/").get(function (req, res) {
     .then((response) => response.json())
     .then((data) => {
       let vals = data.data.viewer.pinnedItems.edges
-      // let stack = Object.values(vals[0].node.languages.nodes)
-      // let tags = Object.values(stack)
-
-      // tags = tags.map((a) => a.name)
 
       let payload = {
         Description: "",
@@ -69,16 +114,10 @@ app.route("/").get(function (req, res) {
         payload[
           `mediaUrl${i}`
         ] = `https://raw.githubusercontent.com/AshakaE/${vals[i].node.name}/main/docs/snapshot.png`
-        // for (let j = 0; j < vals[i].node.languages.nodes.length; j++) {
         let stack = Object.values(vals[i].node.languages.nodes)
         let tags = Object.values(stack)
         tags = tags.map((a) => a.name)
-        // payload[`stacks${i}`] = tags[j].name
-        // let all = []
-        // all.push(tags[j].name)
-        // console.log(all)
         payload[`stacks${i}`] = tags.join(", ")
-        // }
       }
 
       res.render("index", payload)
